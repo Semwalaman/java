2fdad142a02c33dcc3cd248b24525ed305155d0f
Semwalaman <95701180+Semwalaman@users.noreply.github.com>
2022-03-24 21:30:48 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,24 @@
-import java.util.*;
+​import​ ​java.util.*​; 
+  
+ ​public​ ​class​ ​p1_Task01_keywodsInJava​ { 
+ ​    ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) { 
+ ​        ​float​ ​float​ ​=​2​; 
+ ​       ​//​ Syntax error on token "float", invalid VariableDeclaratorIdJava(1610612971) 
+ ​        float(); 
+ ​    } 
+  
+ ​    ​Static​ ​void​ ​float​() 
+ ​    {   ​//​ Syntax error on tokens, delete these tokensJava(1610612969) 
+ ​        ​System​.​out​.​println(​"​hello​"​); 
+ ​    } 
+  
+ ​    abc ​static​ ​=​ ​new​ abc(); 
+ ​    ​//​Syntax error on token "static", invalid VariableDeclaratorIdJava(1610612971) 
+ ​} 
+ ​class​ ​abc 
+ ​{ 
+ ​    ​int​ ​print​() 
+ ​    { 
+ ​        ​System​.​out​.​println(​"​hello​"​); 
+ ​    } 
+ ​}
