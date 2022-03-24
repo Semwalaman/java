9fddb25e57ca94314f78206d9d8772fd8089ce6b
Semwalaman <95701180+Semwalaman@users.noreply.github.com>
2022-03-24 21:40:53 +0530

    Create p2_Task02_constantsInJava.java

---
+++ p2_Task02_constantsInJava.java
@@ -0,0 +1,12 @@
+import​ ​java.util.*​; 
+  
+ ​public​ ​class​ ​p2_Task02_constatntsInJava​ { 
+ ​    ​static​ ​final​ ​float​ pi ​=​ ​3.14f​; ​//​ global constant 
+  
+ ​    ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) { 
+ ​        ​final​ ​int​ r ​=​ ​10​; ​//​ local constatnt 
+ ​        ​float​ area ​=​ pi ​​ r ​​ r; 
+ ​        ​System​.​out​.​println(area); 
+ ​    } 
+  
+ ​}
